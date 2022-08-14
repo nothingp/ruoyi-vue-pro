d5883c66eb728cbcaab4149f8f391c77c78193df
zhoujd <m@zhoujd.me>
2022-08-14 11:31:14 +0800

    feat: 增加sh脚本

--- package.sh
+++ package.sh
@@ -4,5 +4,5 @@ basepath=`pwd`
 
 mvn clean install -Dmaven.test.skip=true
 
-cd $basepath/yudao-server && docker build -t registry.cn-hangzhou.aliyuncs.com/nothing/ruoyi-vue-pro:test .
+cd $basepath/yudao-server && docker build -t registry.cn-hangzhou.aliyuncs.com/nothing/ruoyi-vue-pro:latest .
 
