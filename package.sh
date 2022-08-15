9bf8b60f62cb5a7774600c43a8b2bb0d81de8abc
zhoujd <m@zhoujd.me>
2022-08-15 14:11:29 +0800

    feat: 增加web的docker构建

--- package.sh
+++ package.sh
@@ -4,4 +4,4 @@ basepath=`pwd`
 
 mvn clean install -Dmaven.test.skip=true
 
-cd $basepath/yudao-server && docker build -t ccr.ccs.tencentyun.com/yi_qi_hui_zhan/ruoyi-vue-pro:latest .
+cd $basepath/yudao-server && docker build -t ccr.ccs.tencentyun.com/chang_an/ruoyi-vue-pro:latest .
